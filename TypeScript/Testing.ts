f2461246e913b9435068637119823b6befff61b3
Chua Wei Zhe <chuazhe@gmail.com>
2021-04-16 16:41:26 +0800

    Java-Testing-1_2

---
+++ TypeScript/Testing.ts
@@ -0,0 +1,10 @@
+console.log("aaaaaah");
+
+var x : string;
+var y : number;
+
+x = "abcd";
+y = 2;
+
+console.log(x);
+console.log(y);
---
+++ TypeScript/out/Testing.js
@@ -0,0 +1,8 @@
+console.log("aaaaaah");
+var x;
+var y;
+x = "abcd";
+y = 2;
+console.log(x);
+console.log(y);
+//# sourceMappingURL=Testing.js.map
